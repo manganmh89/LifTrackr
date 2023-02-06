e47e3bedb0a65bfc95db596ac0c70d5772d629a6
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-02-06 16:30:34 -0500

    adds log model, all migrations until signup

--- main_app/models.py
+++ main_app/models.py
@@ -19,6 +19,7 @@ class Workout(models.Model):
     name = models.CharField(max_length=100)
     target = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    exercises = models.ManyToManyField(Exercise)
     
     def __str__(self):
         return self.name
@@ -26,4 +27,22 @@ class Workout(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={'workout_id': self.id})
 
+class Logging(models.Model):
+    EFFORTS = (
+        ('*', '1 Star'),
+        ('**', '2 Star'),
+        ('***', '3 Star'),
+        ('****', '4 Star'),
+        ('*****', '5 Star'),
+    )
 
+    date = models.DateField('workout date')
+    time = models.DateTimeField(auto_now_add=True)
+    effort = models.CharField(max_length=5, choices=EFFORTS, default=EFFORTS[0][0], verbose_name='effort given')
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_effort_display()} on {self.date}"
+
+    class Meta:
+        ordering = ('-date',)
