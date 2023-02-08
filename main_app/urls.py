a6c6d9fa573d6bcc2a45d916d92cdbe84e874225
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-02-08 11:19:33 -0500

    deployment fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('workouts/', views.workouts_index, name='index'),
     path('workouts/<int:workout_id>/', views.workouts_detail, name='detail'),
