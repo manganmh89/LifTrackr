decbf77f45be8505e2c015639944b4e8c56a6df6
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-02-08 10:51:12 -0500

    config deployment rd2

--- workouttracker/settings.py
+++ workouttracker/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django-on-heroku
-django_on_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
