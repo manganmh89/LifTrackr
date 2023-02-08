b0c5269bf64a290dfa8fbb907f006bd1d88c6157
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-02-08 11:01:44 -0500

    adds home route adjustment

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('workouts/', views.workouts_index, name='index'),
     path('workouts/<int:workout_id>/', views.workouts_detail, name='detail'),
