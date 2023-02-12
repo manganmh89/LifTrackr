716f684522e36c44d482786a6df2926ea1f0c0c9
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-02-12 11:00:27 -0500

    adds env and requirements

--- workouttracker/settings.py
+++ workouttracker/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
-load_dotenv() # take environment variables from .env.
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path) # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
